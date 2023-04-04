9cecb198cb022788123239a35c494a911250dc32
Knarik <knazaryan@servicetitan.com>
2023-04-04 18:00:06 +0400

    add new elements

--- cypress/Pages/BasePageElements.ts
+++ cypress/Pages/BasePageElements.ts
@@ -12,4 +12,28 @@ export class BasePageElements {
         return cy.get(`[class*="site-nav"]`).contains(name);
     }
 
+    static headerLogo(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(".site-header__logo-image.site-header__logo-image--centered");
+    }
+
+    static subscribtionField(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(".input-group__field.newsletter__input").last();
+    }
+
+    static subscribeButton(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(".newsletter__submit-text--large").last();
+    }
+
+    static faceBookIcon(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(".icon.icon-facebook");
+    }
+
+    static instagramIcon(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(".icon.icon-instagram");    
+    }
+
+    static subscribtionConfirmation(): Cypress.Chainable<JQuery<HTMLElement>> {
+        return cy.get(".form--success");
+    }
+
 }
