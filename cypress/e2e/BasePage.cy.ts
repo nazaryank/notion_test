bf1118b8d919e4242ea3994b02b6fb746476457e
Knarik <knazaryan@servicetitan.com>
2023-04-04 17:07:17 +0400

    removed unused comment

--- cypress/e2e/BasePage.cy.ts
+++ cypress/e2e/BasePage.cy.ts
@@ -1,10 +1,20 @@
+import { base64StringToBlob } from "blob-util";
 import { BasePageElements } from "../Pages/BasePageElements";
 
 describe('Base Page UI and buttons functionality', () => {
-  it('Should verify base pages UI and existing buttons functionality', () => {
-    cy.visit('https://newtonsupplyco.com/pages/contact');
+
+  it('Should verify base pages UI', () => {
+    cy.visit('https://newtonsupplyco.com');
+    BasePageElements.headerLogo().should("be.visible");
     BasePageElements.searchButton().should("be.visible");
     BasePageElements.shopIcon().should("be.visible");
     BasePageElements.navButtons("Home").should("be.visible");
+    BasePageElements.navButtons("Shop").should("be.visible");
+    BasePageElements.navButtons("About Us").should("be.visible");
+    BasePageElements.navButtons("Stockists").should("be.visible");
+    BasePageElements.navButtons("Custom").should("be.visible");
+    BasePageElements.navButtons("FAQs + Care Info").should("be.visible");
+    BasePageElements.navButtons("Contact").should("be.visible");
   })
+
 })
