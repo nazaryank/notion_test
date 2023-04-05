792fa5a7498c201d7ada13c3af5cef5f799a3811
Knarik <knazaryan@servicetitan.com>
2023-04-05 13:04:53 +0400

    created validation function for nav bar elements

--- cypress/e2e/BasePage.cy.ts
+++ cypress/e2e/BasePage.cy.ts
@@ -1,20 +1,14 @@
 import { base64StringToBlob } from "blob-util";
 import { BasePageElements } from "../Pages/BasePageElements";
+import { Constants } from "../fixtures/Constants";
+import ContactTestBase from "./ContactTestBase.cy";
 
+let baseURL = Constants.baseURL
 describe('Base Page UI and buttons functionality', () => {
 
   it('Should verify base pages UI', () => {
-    cy.visit('https://newtonsupplyco.com');
-    BasePageElements.headerLogo().should("be.visible");
-    BasePageElements.searchButton().should("be.visible");
-    BasePageElements.shopIcon().should("be.visible");
-    BasePageElements.navButtons("Home").should("be.visible");
-    BasePageElements.navButtons("Shop").should("be.visible");
-    BasePageElements.navButtons("About Us").should("be.visible");
-    BasePageElements.navButtons("Stockists").should("be.visible");
-    BasePageElements.navButtons("Custom").should("be.visible");
-    BasePageElements.navButtons("FAQs + Care Info").should("be.visible");
-    BasePageElements.navButtons("Contact").should("be.visible");
+    cy.visit(baseURL);
+    ContactTestBase.validateNavBarElements();
   })
 
 })
