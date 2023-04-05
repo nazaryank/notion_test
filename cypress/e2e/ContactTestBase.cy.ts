8932e28681f4b04c0ecdab9250fb73f352c39356
Knarik <knazaryan@servicetitan.com>
2023-04-05 23:38:18 +0400

    added social media icons verification functions

--- cypress/e2e/ContactTestBase.cy.ts
+++ cypress/e2e/ContactTestBase.cy.ts
@@ -13,4 +13,16 @@ export default class ContactTestBase {
         BasePageElements.navButtons("FAQs + Care Info").should("be.visible");
         BasePageElements.navButtons("Contact").should("be.visible");
     }
+
+    static verifyFacebookIconFunctionality(): void {
+        BasePageElements.faceBookIcon().should("be.visible");
+        BasePageElements.faceBookIcon().click();
+        cy.url().should("eq", "https://www.facebook.com/newtonsupplyco")
+    }
+
+    static verifyInstagramIconFunctionality(): void {
+        BasePageElements.instagramIcon().should("be.visible");
+        BasePageElements.instagramIcon().click();
+        cy.url().should("eq", "https://www.instagram.com/newtonsupplyco/");
+    }
 }
